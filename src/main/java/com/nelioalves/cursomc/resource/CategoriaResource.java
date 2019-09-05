f91fc457e6608893f9b662ada83fc32f964c6124
Walber Junior <wjjunho@hotmail.com>
2019-09-04 22:11:03 -0300

    Primeira class de dominium

--- src/main/java/com/nelioalves/cursomc/resource/CategoriaResource.java
+++ src/main/java/com/nelioalves/cursomc/resource/CategoriaResource.java
@@ -1,15 +1,28 @@
 package com.nelioalves.cursomc.resource;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.nelioalves.cursomc.domain.Categoria;
 
 @RestController
 @RequestMapping(value = "/categorias")
 public class CategoriaResource {
 
 	@RequestMapping(method = RequestMethod.GET)
-	public String listar() {
-		return "Rest está funcionando!";
+	public List<Categoria> listar() {
+		
+		Categoria cat1 = new Categoria(1, "Informatica");
+		Categoria cat2 = new Categoria(2, "Alimentos");
+		
+		List<Categoria> categorias = new ArrayList<Categoria>();
+		categorias.addAll(Arrays.asList(cat1, cat2));
+		
+		return categorias;
 	}
 }
