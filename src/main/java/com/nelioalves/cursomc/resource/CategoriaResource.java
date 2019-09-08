4f4a0065615d712fc7fbf88cfd18f6a98aff85cf
Walber Junior <wjjunho@hotmail.com>
2019-09-08 11:41:21 -0300

    Criação de Recurso Categoria

--- src/main/java/com/nelioalves/cursomc/resource/CategoriaResource.java
+++ src/main/java/com/nelioalves/cursomc/resource/CategoriaResource.java
@@ -1,28 +1,27 @@
 package com.nelioalves.cursomc.resource;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.nelioalves.cursomc.domain.Categoria;
+import com.nelioalves.cursomc.service.CategoriaService;
 
 @RestController
 @RequestMapping(value = "/categorias")
 public class CategoriaResource {
 
-	@RequestMapping(method = RequestMethod.GET)
-	public List<Categoria> listar() {
+	@Autowired
+	private CategoriaService categoriaService;
+	
+	@RequestMapping(value = "/{id}",method = RequestMethod.GET)
+	public ResponseEntity<?> find(@PathVariable Integer id) {
 		
-		Categoria cat1 = new Categoria(1, "Informatica");
-		Categoria cat2 = new Categoria(2, "Alimentos");
+		Categoria cat = categoriaService.find(id);
 		
-		List<Categoria> categorias = new ArrayList<Categoria>();
-		categorias.addAll(Arrays.asList(cat1, cat2));
-		
-		return categorias;
+		return ResponseEntity.ok().body(cat);
 	}
 }
---
+++ src/main/java/com/nelioalves/cursomc/service/CategoriaService.java
@@ -0,0 +1,23 @@
+package com.nelioalves.cursomc.service;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.nelioalves.cursomc.domain.Categoria;
+import com.nelioalves.cursomc.repository.CategoriaRepository;
+
+@Service
+public class CategoriaService {
+
+	@Autowired
+	private CategoriaRepository categoriaRepository;
+	
+	public Categoria find(Integer id) {
+		
+		Optional<Categoria> categoria = categoriaRepository.findById(id);
+		
+		return categoria.orElse(null); 
+	}
+}
